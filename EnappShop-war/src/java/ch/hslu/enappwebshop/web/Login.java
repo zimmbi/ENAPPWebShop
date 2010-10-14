f73213eb5005231490e8fca894b22755faae5ec5
Martin Zimmermann <zimmbi@gmail.com>
2010-10-14 18:02:53 +0200

    new design

---
+++ EnappShop-war/src/java/ch/hslu/enappwebshop/web/Login.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hslu.enappwebshop.web;
+
+import ch.hslu.enappwebshop.entities.Customer;
+
+/**
+ *
+ * @author zimmbi
+ */
+public class Login {
+
+    private String username;
+    private String password;
+    private Customer customer;
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
---
+++ EnappShop-war/src/java/ch/hslu/enappwebshop/web/TestBean.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ch.hslu.enappwebshop.web;
+
+
+/**
+ *
+ * @author zimmbi
+ */
+public class TestBean {
+
+    /** Creates a new instance of TestBean */
+    public TestBean() {
+    }
+
+}
