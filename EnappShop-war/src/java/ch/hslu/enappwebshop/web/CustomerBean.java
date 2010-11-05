a007f46ced91b530912f6bd1d551e136eefd24db
Martin Zimmermann <zimmbi@gmail.com>
2010-11-05 10:58:18 +0100

    asdf

--- EnappShop-war/src/java/ch/hslu/enappwebshop/web/CustomerBean.java
+++ EnappShop-war/src/java/ch/hslu/enappwebshop/web/CustomerBean.java
@@ -14,7 +14,6 @@ import java.util.List;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
-import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 
 import javax.inject.Named;
